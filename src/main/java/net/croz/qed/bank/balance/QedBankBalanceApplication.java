82f36b7c64aa2ffb2fcd44b6fa5d3f838331538c
Milan Jovanovic <mjovanovic@croz.net>
2019-05-09 21:47:26 +0200

    Added Jaeger tracing

--- src/main/java/net/croz/qed/bank/balance/QedBankBalanceApplication.java
+++ src/main/java/net/croz/qed/bank/balance/QedBankBalanceApplication.java
@@ -1,11 +1,21 @@
 package net.croz.qed.bank.balance;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QedBankBalanceApplication {
     public static void main(final String[] args) {
         SpringApplication.run(QedBankBalanceApplication.class, args);
+    }
+
+    @Bean
+    public static JaegerTracer getTracer() {
+        final Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        final Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
+        return new Configuration("qed-bank-balance").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
     }
 }
