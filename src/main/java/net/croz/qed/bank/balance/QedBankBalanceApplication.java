c11fd74e4d31de594a6cb6496617a5df2a8eecb7
Milan Jovanovic <mjovanovic@croz.net>
2023-02-04 14:08:11 +0100

    Removed obsolite files

--- src/main/java/net/croz/qed/bank/balance/QedBankBalanceApplication.java
+++ src/main/java/net/croz/qed/bank/balance/QedBankBalanceApplication.java
@@ -1,7 +1,5 @@
 package net.croz.qed.bank.balance;
 
-import io.jaegertracing.Configuration;
-import io.jaegertracing.internal.JaegerTracer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,13 +10,6 @@ public class QedBankBalanceApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(QedBankBalanceApplication.class, args);
-    }
-
-    @Bean
-    public static JaegerTracer getTracer(@Value("${spring.application.name}") final String applicationName) {
-        final Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
-        final Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv().withLogSpans(Boolean.TRUE);
-        return new Configuration(applicationName).withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
     }
 
 }
